a0017e0b112820693f63b473cc21490d07cfa811
Ire2345 <73399409+Ire2345@users.noreply.github.com>
2022-06-19 18:32:54 +0100

    Add files via upload

---
+++ class 108/normal_distribution.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import plotly.figure_factory as ff
+
+df=pd.read_csv("data3.csv")
+
+fig=ff.create_distplot([df["Height(Inches)"].tolist()],["height"],show_hist=False)
+fig.show()
